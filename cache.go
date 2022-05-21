90c9ca40feb99dd6371c342631c8395d45df794c
Sviatoslav <sviat.v.guss@gmail.com>
2022-05-21 10:52:56 +0600

    hw 5 (cache) is done

--- cache.go
+++ cache.go
@@ -3,21 +3,49 @@ package cache
 import "time"
 
 type Cache struct {
+	values    map[string]string
+	deadlines map[string]time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		values:    map[string]string{},
+		deadlines: map[string]time.Time{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	v, vOk := c.values[key]
+	if !vOk {
+		return "", false
+	}
+	d, dOk := c.deadlines[key]
+	if dOk {
+		if d.Before(time.Now()) {
+			delete(c.values, key)
+			delete(c.deadlines, key)
+			return "", false
+		}
+	}
+	return v, vOk
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.values[key] = value
+	_, ok := c.deadlines[key]
+	if ok {
+		delete(c.deadlines, key)
+	}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var keys = make([]string, 0, len(c.values))
+	for key := range c.values {
+		keys = append(keys, key)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.values[key] = value
+	c.deadlines[key] = deadline
 }
